faa4514798ca9bd665007780fd29c43839b84271
Rafael Flores PEreira <rafaelfp84@gmail.com>
2024-07-16 19:50:10 -0300

    added relay

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,13 @@
+import { RelayEnvironmentProvider } from 'react-relay'
+import { RelayEnvironment } from './RelayEnvironment'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './app'
 import './styles/globals.less'
 import './i18n/config'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />)
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <RelayEnvironmentProvider environment={RelayEnvironment}>
+    <App />
+  </RelayEnvironmentProvider>,
+)
--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import svgr from 'vite-plugin-svgr'
-import path from 'path'
+import relay from "vite-plugin-relay";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: "./",
   plugins: [
+    relay,
     svgr({
       exportAsDefault: true,
     }),
     react(),
   ],
+
   resolve: {
     alias: {
-      src: path.resolve(__dirname, './src'),
+      src: path.resolve(__dirname, "./src"),
     },
   },
   css: {
@@ -28,4 +31,4 @@ export default defineConfig({
     host: true,
     port: 5000,
   },
-})
+});
